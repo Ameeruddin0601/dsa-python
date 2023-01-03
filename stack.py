980fb02d4989d618d1ef66706146e7a801e2ae50
Ameeruddin06 <ameerb0601@gmail.com>
2023-01-03 20:43:01 +0530

    update

--- stack.py
+++ stack.py
@@ -1,5 +1,37 @@
-#Stack implementation
-stack=[]
+#Stack implementation using list
+# stack=[]
+# def push():
+#     if len(stack)==len_stack:
+#         print("Stack is full")
+#     else:
+#         element = input("Enter the element: ")
+#         stack.append(element)
+#     print(stack)
+
+# def pop():
+#     if not stack:
+#         print("Stack is already empty.")
+#     else:
+#         element = stack.pop()
+#         print("Element popped: ",element)
+#         print(stack)
+# len_stack=int(input("Limit of stack: "))
+# while True:  
+#     user_input = int(input("Enter your choice: \n 1. Push\n 2.Pop\n 3.Quit\n ->"))
+#     if user_input==1:
+#         push()
+#     elif user_input==2:
+#         pop()
+#     elif user_input==3:
+#         break
+#     else:
+#         print("Enter the correct choice!")
+
+#Stack implementation using modules
+#collections -> deque
+import collections
+stack=collections.deque()
+
 def push():
     if len(stack)==len_stack:
         print("Stack is full")
@@ -27,3 +59,32 @@ while True:
     else:
         print("Enter the correct choice!")
 
+#queue -> LifoQueue
+import queue
+stack= queue.LifoQueue()
+def push():
+    if len(stack)==len_stack:
+        print("Stack is full")
+    else:
+        element = input("Enter the element: ")
+        stack.put(element)
+    print(stack)
+
+def pop():
+    if not stack:
+        print("Stack is already empty.")
+    else:
+        element = stack.get()
+        print("Element popped: ",element)
+        print(stack)
+len_stack=int(input("Limit of stack: "))
+while True:  
+    user_input = int(input("Enter your choice: \n 1. Push\n 2.Pop\n 3.Quit\n ->"))
+    if user_input==1:
+        push()
+    elif user_input==2:
+        pop()
+    elif user_input==3:
+        break
+    else:
+        print("Enter the correct choice!")
