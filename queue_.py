52a0a94cb650eb7b759331188eb23b1f1d80a03b
Ameeruddin06 <ameerb0601@gmail.com>
2023-01-04 19:41:09 +0530

    q update

--- queue_.py
+++ queue_.py
@@ -26,18 +26,47 @@
 #         print("Enter correct choice.")
 
 #implementation of queue using modules
-import collections
-queue_= collections.deque()
+# import collections
+# queue_= collections.deque()
+
+# def enqueue():
+#     element = input("Enter the element: ")
+#     queue_.appendleft(element)
+#     display()
+# def dequeue():
+#     if not queue_:
+#         print("Queue already empty")
+#     else:
+#         element = queue_.pop()
+#         print("Element removed is: ", element)
+#     display()
+# def display():
+#     print(queue_)
+
+# while True:
+#     user_input=int(input("Enter your choice: \n 1.Enqueue\n 2.Dequeu \n 3.Quit \n ->"))
+#     if user_input==1:
+#         enqueue()
+#     elif user_input==2:
+#         dequeue()
+#     elif user_input==3:
+#         break
+#     else:
+#         print("Enter correct choice.")
+
+#implementation of queue using queue module
+import queue
+queue_= queue.Queue()
 
 def enqueue():
     element = input("Enter the element: ")
-    queue_.appendleft(element)
+    queue_.put_nowait(element)
     display()
 def dequeue():
-    if not queue_:
+    if queue_.empty():
         print("Queue already empty")
     else:
-        element = queue_.pop()
+        element = queue_.get_nowait()
         print("Element removed is: ", element)
     display()
 def display():
