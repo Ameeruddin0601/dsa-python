9ecbfcc725be454ab4fb309eb4074c2527f89df1
Ameeruddin06 <ameerb0601@gmail.com>
2023-01-10 22:12:25 +0530

    Update

--- linkedList.py
+++ linkedList.py
@@ -8,10 +8,30 @@ class LinkedList:
         self.head = None
 
     def print_list(self):
-        temp = self.head
-        while temp!=None:
-            print(temp.data)
-            temp = temp.next
+        if self.head is None:
+            print("Linked List is empty")
+        else:
+            temp = self.head
+            while temp!=None:
+                print(temp.data, end = " > ")
+                temp = temp.next
+    
+    #push node at start of Linked List
+    def push_begin(self,data):
+        NewNode = Node(data)
+        NewNode.next = self.head
+        self.head = NewNode
+
+    #push node at end of Linked List
+    def push_end(self,data):
+        NewNode = Node(data)
+        if self.head is None:
+            self.head = NewNode
+        else:
+            temp = self.head
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = NewNode
 
 myList = LinkedList()
 first = Node(10)
@@ -22,4 +42,10 @@ myList.head = first
 first.next = second
 second.next =third
 
+#myList.print_list()
+myList.push_begin(5)
+myList.push_end(40)
 myList.print_list()
+
+#push node at any position of LL
+#delete node at any position of LL
