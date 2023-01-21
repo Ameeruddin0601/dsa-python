0293072c8e27a01ffd6e33480762bcee297733b8
Ameeruddin06 <ameerb0601@gmail.com>
2023-01-21 20:33:34 +0530

    Update

--- doublyll.py
+++ doublyll.py
@@ -99,12 +99,54 @@ class DoublyLL:
                     temp.prev = NewNode
 
 
-    def del_first(data):
-        pass
-    def del_last(data):
-        pass
-    def del_node(data):
-        pass
+    def del_first(self):
+        if self.head is None:
+            print("Linked List is empty")
+        elif self.head.next is None:
+            self.head = None
+            print("Node deleted. List is empty now")
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+
+    def del_last(self):
+        if self.head is None:
+            print("Linked List is empty")
+        elif self.head.next is None:
+            self.head = None
+            print("Node deleted. List is empty now")
+        else:
+            temp = self.head
+            while temp.next is not None:
+                temp = temp.next
+            
+            temp.prev.next = None
+
+    def del_node(self,data):
+        if self.head is None:
+            print("Linked List is empty")
+            return
+        if self.head.next is None:
+            self.head = None
+            print("Node deleted. List is empty now")
+        else:
+            temp = self.head
+            while temp is not None:
+                if temp.data == data:
+                    break
+                temp = temp.next
+            if temp is None:
+                print("Node not found")
+            else:
+                if temp.prev is None:
+                    self.del_first()
+                elif temp.next is None:
+                    self.del_last()
+                else:                
+                    temp.prev.next = temp.next
+                    temp.next.prev = temp.prev
+                    temp.next = None
+                    temp.prev = None
 
 myList = DoublyLL()
 first = Node(10) # myList.push_empty(10)
@@ -124,5 +166,8 @@ myList.push_end(40)
 myList.push_afterNode(15,10)
 myList.push_afterNode(50,40)
 myList.push_beforeNode(2.5,5)
-myList.push_beforeNode(60,50)
+myList.push_beforeNode(45,50)
+myList.del_first()
+myList.del_last()
+myList.del_node(15)
 myList.print_dll()
